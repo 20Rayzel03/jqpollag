cb9bf6029ce1abff4465e28742b2bfcc0b9fddc9
Highlander1556 <heilerdesgulasch.lost@gmail.com>
2025-05-24 20:56:41 +0200

    enum for pages

--- julius-website/app/enums/PAGES.tsx
+++ julius-website/app/enums/PAGES.tsx
@@ -1,8 +1,27 @@
-
 enum PAGES {
     HOME = "/",
     LOGIN = "/login",
     Blog = "/blog",
     ADMIN = "/admin",
-    
+
 }
+
+function handlePage(page: PAGES) {
+    switch (page) {
+        case PAGES.HOME:
+            // Handle home page
+            return "/";
+        case PAGES.LOGIN:
+            // Handle login page
+            return "/login";
+        case PAGES.Blog:
+            // Handle blog page
+            return "/blog";
+        case PAGES.ADMIN:
+            // Handle admin page
+            return "/admin";
+        default:
+            return "Unknown Page";
+    }
+}
+
